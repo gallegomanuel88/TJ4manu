25af759faa2a5b0af298e015c9b9ef306cfc448a
galle <galle@DESKTOP-J6RSNG6>
2016-04-18 09:09:04 +0200

    Instanciado.

---
+++ src/tj4/TJ4.java
@@ -0,0 +1,16 @@
+package tj4;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+/**
+ *
+ * @author galle
+ */
+public class TJ4 {
+
+    public static void main(String[] args) {
+        Twitter mitwitter = new TwitterFactory ().getInstance();
+    }
+    
+}
