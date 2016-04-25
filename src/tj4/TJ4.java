5102fd6aa26dd26c69b20afb50775289508cbf21
galle <galle@DESKTOP-J6RSNG6>
2016-04-25 09:31:12 +0200

    Terminado

--- src/tj4/TJ4.java
+++ src/tj4/TJ4.java
@@ -1,16 +1,23 @@
 package tj4;
 
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+
 
 /**
  *
- * @author galle
+ * @author gallegomanuel
  */
 public class TJ4 {
 
-    public static void main(String[] args) {
-        Twitter mitwitter = new TwitterFactory ().getInstance();
+    public static void main(String[] args) throws TwitterException {
+        Twitter miTwitter = new TwitterFactory("twitter4j.properties").getInstance();
+        Status miStatus = miTwitter.updateStatus("Holi");
+        System.out.println(miStatus.getText());
     }
     
 }
